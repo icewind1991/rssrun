1f9db29e72a9e0e846b80ce34b0249d1fad9f240
Robin Appelman <icewind@owncloud.com>
2015-09-13 14:19:36 +0200

    fix exec usage

--- runner.js
+++ runner.js
@@ -8,7 +8,7 @@ var config = require('./config.json');
 emitter.on('item:new', function (guid, item) {
 	var command = interpolate(config.run, item);
 	console.log("running: " + command);
-	exec(command);
+	exec(command, function(){});
 });
 
 emitter.on('item:skipped', function (guid) {
